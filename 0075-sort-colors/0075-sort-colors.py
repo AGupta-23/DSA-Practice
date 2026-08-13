69585a9f5a305e139c5c26a5cc0fcb34cb5c15df
AGupta-23 <166648588+AGupta-23@users.noreply.github.com>
2026-08-13 21:50:17 +0530

    Time: 0 ms (100%), Space: 19.2 MB (61.53%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -1,22 +1,18 @@
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
         n=len(nums)
-        count0,count1,count2=0,0,0
-        #traversal
-        for i in range(0,n):
-            if nums[i]==0: count0+=1
-            elif nums[i]==1: count1+=1
-            else: count2+=1
 
-        inx=0
+        low,mid,high=0,0,n-1
 
-        for i in range(0,count0):
-            nums[inx]=0
-            inx+=1
-        for i in range(0,count1):
-            nums[inx]=1
-            inx+=1
-        for i in range(0,count2):
-            nums[inx]=2
-            inx+=1
+        while(mid<=high):
+
+            if nums[mid]==0:
+                nums[low],nums[mid]=nums[mid],nums[low]
+                low+=1
+                mid+=1
+            elif nums[mid]==1:
+                mid+=1
+            else: #nums[mid]=2
+                nums[mid],nums[high]=nums[high],nums[mid]
+                high-=1
 
