2cef881267f9cc846422dcb5a8c8b1cb25043f41
AGupta-23 <166648588+AGupta-23@users.noreply.github.com>
2026-08-17 10:09:31 +0530

    Time: 3168 ms (5.02%), Space: 24 MB (5.35%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,41 +1,21 @@
 class Solution:
     def threeSum(self, nums: list[int]) -> list[list[int]]:
 
-        nums.sort()
         n = len(nums)
-        ans = []
+        result_set = set()
 
-        for i in range(n):
-
-            # Skip duplicate first elements
-            if i > 0 and nums[i] == nums[i - 1]:
-                continue
-
-            left = i + 1
-            right = n - 1
-
-            while left < right:
-
-                total = nums[i] + nums[left] + nums[right]
-
-                if total < 0:
-                    left += 1
-
-                elif total > 0:
-                    right -= 1
-
-                else:
-                    ans.append([nums[i], nums[left], nums[right]])
-
-                    left += 1
-                    right -= 1
-
-                    # Skip duplicate left values
-                    while left < right and nums[left] == nums[left - 1]:
-                        left += 1
-
-                    # Skip duplicate right values
-                    while left < right and nums[right] == nums[right + 1]:
-                        right -= 1
-
-        return ans
+        for i in range(0,n):
+            my_set=set()
+            for j in range(i+1,n):
+                third = -(nums[i]+nums[j])
+                if third in my_set:
+                    temp = [nums[i],nums[j],third]
+                    temp.sort()
+                    result_set.add(tuple(temp))
+                my_set.add(nums[j])
+        
+        ansList=[]
+        for x in result_set:
+            ansList.append(list(x))
+        
+        return ansList
