4f5553297455ddf4d1d176cf9af4069ffcae3b1a
AGupta-23 <166648588+AGupta-23@users.noreply.github.com>
2026-08-29 22:54:48 +0530

    Time: 0 ms (100%), Space: 19.4 MB (17.45%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -0,0 +1,12 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        one = 1
+        two = 1
+
+        for i in range(2, n + 1):
+            current = one + two
+            two = one
+            one = current
+
+        return one
+        
