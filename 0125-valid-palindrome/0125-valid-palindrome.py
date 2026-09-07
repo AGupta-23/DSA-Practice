97afbb21c39a2b80237ca66d92d1eea11edc3507
AGupta-23 <166648588+AGupta-23@users.noreply.github.com>
2026-09-07 19:59:16 +0530

    Time: 4 ms (90.44%), Space: 19.5 MB (94.4%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,13 +1,22 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
+        n = len(s)
+        l = 0
+        r = n-1
 
-        newStr = ""
+        while(l<r):
 
-        for char in s:
-            if char.isalnum():
-                newStr += char.lower()
-        reversedStr = newStr[::-1]
-        if newStr == reversedStr:
-            return True
-        else: return False
+            while l<r and not s[l].isalnum(): 
+                l+=1
+
+            while l<r and not s[r].isalnum(): 
+                r-=1
+
+            if s[l].lower() != s[r].lower():
+                return False
+            l+=1
+            r-=1
+        return True
+
+        
         
