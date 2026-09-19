4b924b98c607d03b2eff2035a17d8a6c9fcf1dad
AGupta-23 <166648588+AGupta-23@users.noreply.github.com>
2026-09-19 15:39:14 +0530

    Time: 211 ms (47.27%), Space: 20 MB (31.19%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -3,12 +3,15 @@ class Solution:
 
         n = len(s)
         maxi = 0
-
-        for i in range(0,n):
-            hash_set = set()
-            for j in range(i,n):
-                if s[j] in hash_set: break
-                hash_set.add(s[j])
-                maxi = max(maxi, j-i+1)
+        left , right = 0,0 
+        my_dict = {}
+        
+        while right < n:
+            if s[right] in my_dict:
+                left = max(left, my_dict[s[right]] + 1)
+            length = right - left + 1
+            my_dict[s[right]] = right
+            maxi = max(maxi, length)
+            right+=1
         return maxi
         
