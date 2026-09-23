b5a35a3353162abdd4e192d8928b3108a124a738
AGupta-23 <166648588+AGupta-23@users.noreply.github.com>
2026-09-23 11:32:37 +0530

    Time: 161 ms (86.55%), Space: 25.9 MB (35.24%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.py
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.py
@@ -1,22 +1,21 @@
 class Solution:
-    def totalFruit(self, fruits: List[int]) -> int:
-        n=len(fruits)
-        maxi=0
-        right,left=0,0
-        count={}
+    def totalFruit(self, fruits: list[int]) -> int:
+        n = len(fruits)
+        l = 0
+        maxi = 0
+        count = {}
 
-        for right in range(0,n):
-            count[fruits[right]]=count.get(fruits[right],0) + 1
+        for r in range(0,n):
+            count[fruits[r]] = count.get(fruits[r], 0) + 1
 
-            if len(count)>2:
-                count[fruits[left]]-=1
-                if count[fruits[left]]==0:
-                    del count[fruits[left]]
-                left+=1
-            if len(count)<=2:
-                maxi=max(maxi, right-left+1)
-
+            if len(count) > 2:
+                count[fruits[l]] -= 1
+                if count[fruits[l]] == 0:
+                    del count[fruits[l]]
+                l+=1
+            
+            if len(count) <= 2:
+                maxi = max(maxi, (r-l+1))
         return maxi
 
-                
         
