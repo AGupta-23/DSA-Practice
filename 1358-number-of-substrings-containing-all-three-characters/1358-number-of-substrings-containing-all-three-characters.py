31c6d6b5fa54ac3289f376ccb22a16984b3ba271
AGupta-23 <166648588+AGupta-23@users.noreply.github.com>
2026-09-24 13:50:13 +0530

    Time: 102 ms (48.2%), Space: 19.3 MB (97.64%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.py
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.py
@@ -1,16 +1,16 @@
 class Solution:
     def numberOfSubstrings(self, s: str) -> int:
         n = len(s)
-        hashset = {'a':0, 'b':0, 'c':0}
+        hasharray = [0] * 3
         count = 0
         l = 0
         for r in range(0,n):
-            hashset[s[r]] = hashset.get(s[r], 0) + 1
+            hasharray[ (ord(s[r]) - ord('a')) ] += 1
 
-            while hashset['a']>0 and hashset['b']>0 and hashset['c']>0 :
+            while hasharray[0]>0 and hasharray[1]>0 and hasharray[2]>0 :
                 count += (n-r)
                 
-                hashset[s[l]] -= 1
+                hasharray[ord(s[l]) - ord('a')] -= 1
                 l+=1
         return count
 
